d947c14acda0d6a575987a85a0bec98e45b9f2b2
raghavmahajan <raghav1.mahajan@paytmmoney.com>
2020-06-09 00:41:32 +0530

    webpack-config

--- build-utils/webpack.config.production.js
+++ build-utils/webpack.config.production.js
@@ -1,23 +1,55 @@
-const path = require('path');
-const cdnUploader = require('evive-infra');
-const WebpackOnBuildPlugin = require('on-build-webpack');
-
-const CONTAINER_URL = '';
-const BUILD_NUMBER = process.env.BUILD_NUMBER;
-const BUILD_DIR = path.join(__dirname, '..', 'src/dist/');
-const CDNOPTIONS = Object.freeze({
-  build_env: process.env.buildEnvironment,
-  build_dir: BUILD_DIR,
-  build_number: BUILD_NUMBER,
-  container_name: '',
-});
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
 module.exports = () => ({
-  plugins: [new WebpackOnBuildPlugin(() => {
-    cdnUploader(JSON.stringify(CDNOPTIONS));
-  })],
+  mode: 'production',
   output: {
-    publicPath: `${CONTAINER_URL}/${BUILD_NUMBER}/`,
+    filename: 'js/[name].[contentHash].js',
+    chunkFilename: 'js/[name].[contentHash].js',
+  },
+  optimization: {
+    usedExports: true,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 80 * 1024,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        common: {
+          test: /[\\/]common[\\/]/,
+          priority: 0,
+          minSize: 20 * 1024,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contentHash].css',
+    }),
+  ],
+  performance: {
+    hints: 'warning',
+    // @TODO : to work on this to decrease size
+    maxEntrypointSize: 120 * 1024,
+    assetFilter(assetFilename) {
+      return (assetFilename.endsWith('.js') || assetFilename.endsWith('.css')) && assetFilename.indexOf('vendors') === -1;
+    },
   },
 });
